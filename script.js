bf8a13ef7cc7cf9255827c1d2623cbcdd026191f
MohdOves <mohammadovescontact@gmail.com>
2024-12-18 12:16:28 +0530

    My Portfolio

--- script.js
+++ script.js
@@ -30,10 +30,6 @@ function openTab(tabName) {
     document.getElementById(tabName + '-tab').classList.add('active-tab');
 }
 
-
-;
-
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
